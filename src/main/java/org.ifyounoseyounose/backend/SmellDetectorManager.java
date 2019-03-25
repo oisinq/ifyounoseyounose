8cf428827e424001736eaeccdad652bb3103715f
Oisin Quinn <oisin1001@gmail.com>
2019-03-25 22:50:12 +0000

    Added basic template code for SmellDetectorManager, with comments detailing how to implement the rest

--- src/main/java/org.ifyounoseyounose/backend/SmellDetectorManager.java
+++ src/main/java/org.ifyounoseyounose/backend/SmellDetectorManager.java
@@ -1,4 +1,19 @@
 package org.ifyounoseyounose.backend;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SmellDetectorManager {
+    public List<Result> detectSmells(List<SmellDetector> smellDetectors, List<File> files) {
+        List<Result> results = new ArrayList<>();
+
+        for (File file : files) {
+            for (SmellDetector smellDetector : smellDetectors) {
+                // Check what type of smellDetector it is by seeing what interface it inherits
+                // and call smellDetector.detectSmell() on it with the parameters corresponding to its interface
+            }
+        }
+        return results;
+    }
 }
---
+++ src/test/java/org/ifyounoseyounose/backend/SmellDetectorManagerTest.java
@@ -0,0 +1,18 @@
+package org.ifyounoseyounose.backend;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SmellDetectorManagerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void detectSmells() {
+
+    }
+}
