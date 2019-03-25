0a07b3156d32df20a6ef27f8482bafcea81b3ab1
Oisin Quinn <oisin1001@gmail.com>
2019-03-25 20:09:53 +0000

    Added JUnit, one random test case and .travis.yml

---
+++ src/test/java/org/ifyounoseyounose/javaparsertest/RandomTest.java
@@ -0,0 +1,17 @@
+package org.ifyounoseyounose.javaparsertest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RandomTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
