1e7a4894db5ee9c968e25f8b341d3f12dad585dc
charliekelly13 <charleskelly13@gmail.com>
2019-03-27 12:34:24 +0000

    Interface changes
    Reflection alternate method
    Superclass for smelldetector

--- src/main/java/org.ifyounoseyounose/backend/ReflectionSmellDetector.java
+++ src/main/java/org.ifyounoseyounose/backend/ReflectionSmellDetector.java
@@ -1,6 +1,10 @@
 package org.ifyounoseyounose.backend;
 
+import java.io.File;
+
 public interface ReflectionSmellDetector {
     // This might not be a Reflection class that it's importing, so there may be compatibility issues, so watch out.
     SmellReport detectSmell(Class sourceCode);
+
+    SmellReport detectSmell(File sourceCode);// In case its not a Reflection class
 }
--- src/main/java/org.ifyounoseyounose/backend/SmellDetector.java
+++ src/main/java/org.ifyounoseyounose/backend/SmellDetector.java
@@ -1,4 +1,13 @@
 package org.ifyounoseyounose.backend;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SmellDetector {
+    List<String> smellyCode = new ArrayList<>();// List of code with issues
+
+    private List<String> returnCode() {// Returns the bad code
+        return smellyCode;
+    }
+
 }
