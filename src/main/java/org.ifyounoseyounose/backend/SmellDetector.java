a6f2917c28c32aeff410031c1029e7139e7a37b6
charliekelly13 <charleskelly13@gmail.com>
2019-03-27 12:40:06 +0000

    Removed returnCode method

--- src/main/java/org.ifyounoseyounose/backend/SmellDetector.java
+++ src/main/java/org.ifyounoseyounose/backend/SmellDetector.java
@@ -6,8 +6,4 @@ import java.util.List;
 public class SmellDetector {
     List<String> smellyCode = new ArrayList<>();// List of code with issues
 
-    private List<String> returnCode() {// Returns the bad code
-        return smellyCode;
-    }
-
 }
