377cc79349720a62dfe78d2bf72d8cd30df9f810
charliekelly13 <charleskelly13@gmail.com>
2019-03-27 17:23:31 +0000

    Smellreport and smelldetector reworked

--- src/main/java/org.ifyounoseyounose/backend/SmellDetector.java
+++ src/main/java/org.ifyounoseyounose/backend/SmellDetector.java
@@ -3,7 +3,7 @@ package org.ifyounoseyounose.backend;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SmellDetector {
-    List<ClassReport> smellyCode = new ArrayList<>();// List of code with issues
+abstract class SmellDetector {
+    List<SmellReport> smellyCode = new ArrayList<>();// List of code with issues
 
 }
