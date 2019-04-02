a92957ad11286e9bf148468a9b375631086a7343
SineadFarrell <sinead.farrell1@ucdconnect.ie>
2019-04-02 15:44:12 +0100

    ReportBuilder - generate report

--- src/main/java/org.ifyounoseyounose/backend/ReportBuilder.java
+++ src/main/java/org.ifyounoseyounose/backend/ReportBuilder.java
@@ -1,4 +1,49 @@
 package org.ifyounoseyounose.backend;
 
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+
+
+
 public class ReportBuilder {
+
+    public ArrayList<File> java_files = new ArrayList<>();
+
+    public HashMap<File, HashMap<SmellDetector, List<Integer>>> generateReport(List<SmellDetector> smells, File directory) {
+
+        HashMap<SmellDetector, List<Integer>> linesMap = new HashMap<>();
+        HashMap<File, HashMap<SmellDetector, List<Integer>>> detectMap = new HashMap<>();
+
+        File[] directory_files = directory.listFiles();
+        ArrayList<File> javaFiles;
+        javaFiles = getJavaFiles(directory_files);
+
+      //    for(File file: javaFiles){
+      //       System.err.println(file);
+      //  }
+
+        SmellDetectorManager manager = new SmellDetectorManager();
+        manager.detectSmells(smells, javaFiles);
+
+        // detectMap.put(, linesMap);
+        return detectMap;
+    }
+
+    //getting all the files in the directory that are .java files
+    private ArrayList<File> getJavaFiles(File[] directory) {
+
+        for (File dir : directory) {
+            if (dir.isDirectory()) {
+
+                getJavaFiles(dir.listFiles());
+
+            } else if (dir.isFile() && dir.getName().endsWith(".java")) {
+                java_files.add(dir);
+            }
+        }
+        return java_files;
+    }
+
 }
---
+++ src/test/java/org/ifyounoseyounose/backend/ReportBuilderTest.java
@@ -0,0 +1,31 @@
+package org.ifyounoseyounose.backend;
+
+import org.ifyounoseyounose.backend.smelldetectors.PlaceholderSmellDetector;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ReportBuilderTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void detectSmells() {
+        ReportBuilder r = new ReportBuilder();
+
+        File directory = new File("./src/test/java/smellycodedirectory/");
+
+        List<SmellDetector> smellDetectors = new ArrayList<>();
+        smellDetectors.add(new PlaceholderSmellDetector());
+
+        r.generateReport(smellDetectors, directory);
+        assert(true);
+    }
+}
