9121dea4c5028fb9f4e57350b65e7a630af69c4a
SineadFarrell <sinead.farrell1@ucdconnect.ie>
2019-04-03 17:26:20 +0100

    Implementing Primitive Obsession Smell Detector

---
+++ src/main/java/org.ifyounoseyounose/backend/smelldetectors/PrimitiveObsessionCollector.java
@@ -0,0 +1,26 @@
+package org.ifyounoseyounose.backend.smelldetectors;
+
+import com.github.javaparser.Range;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.type.PrimitiveType;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+import java.util.List;
+import java.util.Optional;
+
+public class PrimitiveObsessionCollector extends VoidVisitorAdapter<List<Integer>> {
+
+    @Override
+    public void visit(PrimitiveType md, List<Integer> collector){
+        super.visit(md, collector);
+        addLineNumbers(md, collector);
+    }
+
+    private void addLineNumbers(Node node, List<Integer> collector) {
+        Optional<Range> m = node.getRange();
+        Range r = m.get();
+        for (int lineNumber = r.begin.line; lineNumber <= r.end.line; lineNumber++) {
+            collector.add(lineNumber);
+        }
+    }
+}
---
+++ src/main/java/org.ifyounoseyounose/backend/smelldetectors/PrimitiveObsessionSmellDetector.java
@@ -0,0 +1,31 @@
+package org.ifyounoseyounose.backend.smelldetectors;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+import org.ifyounoseyounose.backend.JavaParserSmellDetector;
+import org.ifyounoseyounose.backend.SmellDetector;
+import org.ifyounoseyounose.backend.SmellReport;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class PrimitiveObsessionSmellDetector extends SmellDetector implements JavaParserSmellDetector {
+
+
+    @Override
+    public SmellReport detectSmell(HashMap<CompilationUnit, File> compilationUnits) {
+        SmellReport smellReport = new SmellReport();
+        VoidVisitor<List<Integer>> visitor = new PrimitiveObsessionCollector();
+
+        for (CompilationUnit compilationUnit : compilationUnits.keySet()) {
+            List<Integer> collector = new ArrayList<>();
+            visitor.visit(compilationUnit, collector);
+            System.err.println(compilationUnits.get(compilationUnit) + " - " + collector.size());
+            smellReport.addToReport(compilationUnits.get(compilationUnit),collector);
+        }
+
+        return smellReport;
+    }
+}
