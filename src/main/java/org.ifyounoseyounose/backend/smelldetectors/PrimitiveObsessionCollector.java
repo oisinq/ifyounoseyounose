04053bf5f93e7c56714eb4cba48c6714d4c5ed5c
SineadFarrell <sinead.farrell1@ucdconnect.ie>
2019-04-03 18:29:48 +0100

    Primitive Obsesssion - getting map of each type

--- src/main/java/org.ifyounoseyounose/backend/smelldetectors/PrimitiveObsessionCollector.java
+++ src/main/java/org.ifyounoseyounose/backend/smelldetectors/PrimitiveObsessionCollector.java
@@ -5,6 +5,9 @@ import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.type.PrimitiveType;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
+import java.lang.reflect.Type;
+import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +17,26 @@ public class PrimitiveObsessionCollector extends VoidVisitorAdapter<List<Integer
     public void visit(PrimitiveType md, List<Integer> collector){
         super.visit(md, collector);
         addLineNumbers(md, collector);
+    }
+
+    //get the number of each primitive type in a hashMap
+    public HashMap<Type,Integer> reflectPrimitive(Class host){
+        HashMap<Type, Integer> primitiveMap = new HashMap<>();
+
+        Field[]  fields = host.getDeclaredFields();
+        for(Field f:fields){
+            if(f.getType().isPrimitive()){
+
+                if(!primitiveMap.containsKey(f.getType())){
+                    primitiveMap.put(f.getType(), 1);
+                }
+                else{
+                    primitiveMap.put(f.getType(), primitiveMap.get(f.getType())+1);
+                }
+            }
+        }
+
+            return primitiveMap;
     }
 
     private void addLineNumbers(Node node, List<Integer> collector) {
