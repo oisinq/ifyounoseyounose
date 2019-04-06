2eb029ca00f5ba1e0d7b420d4dd201f8ffbe2d2f
Oisin Quinn <oisin1001@gmail.com>
2019-04-06 22:41:23 +0100

    Added test cases for switch statement detection

---
+++ src/test/java/org/ifyounoseyounose/backend/smelldetector/SwitchStatementTest.java
@@ -0,0 +1,28 @@
+package org.ifyounoseyounose.backend.smelldetector;
+
+import org.ifyounoseyounose.backend.SmellDetectorManager;
+import org.ifyounoseyounose.backend.smelldetectors.SmellDetector;
+import org.ifyounoseyounose.backend.smelldetectors.TooManyLiteralsSmellDetector;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SwitchStatementTest {
+
+    @Test
+    public void detectSmells() {
+        SmellDetectorManager s = new SmellDetectorManager();
+
+        List<File> l = new ArrayList<>();
+        l.add(new File("./src/test/java/smellycodedirectory/SmellySwitchStatements.java"));
+
+        List<SmellDetector> smellDetectors = new ArrayList<>();
+        smellDetectors.add(new TooManyLiteralsSmellDetector());
+
+        s.detectSmells(smellDetectors, l);
+        assert(true);
+    }
+}
