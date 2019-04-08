861302629f7b1704dddb9f78991423e7d990ba85
charliekelly13 <charleskelly13@gmail.com>
2019-04-08 17:48:39 +0100

    Smell detector finsihed

--- src/main/java/org.ifyounoseyounose/backend/smelldetectors/DuplicateCodeSmellDetector.java
+++ src/main/java/org.ifyounoseyounose/backend/smelldetectors/DuplicateCodeSmellDetector.java
@@ -7,12 +7,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class DuplicateCodeSmellDetector extends SmellDetector implements ManualParserSmellDetector {
+public class DuplicateCodeSmellDetector extends SmellDetector, LimitableSmellDetector implements ManualParserSmellDetector  {
     @Override
     public SmellReport detectSmell(List<File> sourceCode) {
         SmellReport smells = new SmellReport();// To be returned
-        int count = 0;// Line number
-        HashMap<String, HashMap<File, List<Integer>>> temp = new HashMap<>();// Key is line contents, value is number of times line has appeared
+        int count = 1;// Line number
+        HashMap<String, HashMap<File, List<Integer>>> temp = new HashMap<>();//A hashmap with key being the contents of the line, the value being a hashmap with the key being the file and the value being the line number the content appeared on
 
         for(File f : sourceCode) {// Iterates through files
 
@@ -28,7 +28,7 @@ public class DuplicateCodeSmellDetector extends SmellDetector implements ManualP
                     line = line.trim();
 
                     if (!line.equals("}") && !line.equals("{") && !line.equals("")) {// Checks lines are irrelevant
-                        HashMap<File, List<Integer>> hold = temp.get(line); //see if you already have a list for current key
+                        HashMap<File, List<Integer>> hold = temp.get(line); //see if you already have a hashmap for current key
                         System.out.println("test4");
 
                         if (hold == null) { //if not create one and put it in the map
@@ -42,9 +42,7 @@ public class DuplicateCodeSmellDetector extends SmellDetector implements ManualP
                             l = new ArrayList<Integer>();
                             temp.get(line).put(f, l);
                         }
-                        System.out.println("test1");
-                        temp.get(line).get(f).add(count);
-                        System.out.println("test2");
+                        temp.get(line).get(f).add(count);//Adds the line number to the inner hashmap
                     }
                     count++;
                 }
@@ -60,22 +58,17 @@ public class DuplicateCodeSmellDetector extends SmellDetector implements ManualP
 
         }
         if(!temp.isEmpty()) {//Ensures there is a line of code to add
-            for(HashMap<File, List<Integer>> x: temp.values()) {//Searches for lines that have occurred more than the limit
-                for(File u: x.keySet())
+
+            int maxLines=0;
+            for(HashMap<File, List<Integer>> x: temp.values()) {//Searches through the hashmaps
+                maxLines=0;
+                for(List<Integer> u: x.values())//Adds up the total amount of times the lines has appeared
                 {
-                    System.out.println(u);
+                    maxLines=maxLines+u.size();
                 }
-
-            }
-            System.out.println("break");
-            for(HashMap<File, List<Integer>> x: temp.values()) {//Searches for lines that have occurred more than the limit
-
-                for(File y: sourceCode)
-                {
-                    System.out.println(y);
-                    if(x.get(y).size()>=2)
-                    {
-                        smells.addToReport(y, x.get(y));
+                if(maxLines>=limit) {//If greater then the limit add to report
+                    for (File y : x.keySet()) {
+                            smells.addToReport(y, x.get(y));
                     }
                 }
             }
