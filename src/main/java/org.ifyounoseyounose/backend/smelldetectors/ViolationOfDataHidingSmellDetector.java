ee42f12ba2ea3b43687ead26f437682059cb236a
SineadFarrell <sinead.farrell1@ucdconnect.ie>
2019-04-08 21:02:40 +0100

    ViolationOfDataHidingSmellDetector working

--- src/main/java/org.ifyounoseyounose/backend/smelldetectors/ViolationOfDataHidingSmellDetector.java
+++ src/main/java/org.ifyounoseyounose/backend/smelldetectors/ViolationOfDataHidingSmellDetector.java
@@ -19,41 +19,48 @@ public class ViolationOfDataHidingSmellDetector extends SmellDetector implements
 
 
         SmellReport smells = new SmellReport();
-        List<Integer> lines = new ArrayList<>(); //list of line numbers where smell was found
+
         //count number of lines
         //System.out.println(classes.toString());
-        System.out.println(classes);
+        //System.out.println(classes);
         for (Class current : classes.keySet()) {    //iterate through all the classes in the hashmap
-
-            int count = 1;
-            Field[] fields = current.getDeclaredFields();
-            for(Field f: fields){
+        List<String> publicFields = new ArrayList<>();
+        List<Integer> lines = new ArrayList<>(); //list of line numbers where smell was found
+             Field[] fields = current.getDeclaredFields();
+            for(Field f: fields) {
                 int modifiers = f.getModifiers();
 
-               // System.out.println(f);
-                if(Modifier.isPublic(modifiers)){  //check that the field is public
-                    //System.out.println(f.toString());
+                // System.out.println(f);
+                if (Modifier.isPublic(modifiers)) {  //check that the field is public
+                    //System.out.println(f.getName());
+                    publicFields.add(f.getName());
+                }
                     try {
+                        int count = 1;
                         FileReader targetStream = new FileReader(classes.get(current));
                         //System.out.println(classes.get(current));
                         BufferedReader bufferedReader =
                                 new BufferedReader(targetStream);
                         String line = bufferedReader.readLine();
-                        while(line  != null) {
-                            if(line.startsWith("public")) {
+
+                        while (line != null) {
+                            for (String name : publicFields){
+                            if (line.contains(name) ){
+                                //System.out.println(line);
+                                publicFields.remove(name);
                                 lines.add(count);
+                            }
                             }
                             line = bufferedReader.readLine();
                             count++;
                         }
 
-                    }
-                    catch(Exception e)
-                    {
+                    } catch (Exception e) {
+                        
                         System.out.println("Invalid file");
                     }
 
-                }
+
 
 
                 }
