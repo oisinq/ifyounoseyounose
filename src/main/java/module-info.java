56d9e1dab33218f4665a4e9fbfaa759b522db9e8
Oisin Quinn <oisin1001@gmail.com>
2019-04-09 16:47:52 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,6 @@ module org.ifyounoseyounose {
     requires java.compiler;
     requires com.github.javaparser.core;
     requires  guava;
-    requires javassist;
 
     opens org.ifyounoseyounose to javafx.fxml;
     exports org.ifyounoseyounose;
