019c98e6f945aa4f968d50c4cf3ccf5a406ec2b1
Oisin Quinn <oisin1001@gmail.com>
2019-04-09 17:05:45 +0100

    Removed un-needed import, refactored ViolationOfDataHidingSmellDetector

--- src/main/java/org.ifyounoseyounose/backend/smelldetectors/ViolationOfDataHidingSmellDetector.java
+++ src/main/java/org.ifyounoseyounose/backend/smelldetectors/ViolationOfDataHidingSmellDetector.java
@@ -1,7 +1,5 @@
 package org.ifyounoseyounose.backend.smelldetectors;
 
-
-import javassist.*;
 import org.ifyounoseyounose.backend.SmellReport;
 
 import java.lang.reflect.Modifier;
@@ -17,53 +15,47 @@ public class ViolationOfDataHidingSmellDetector extends SmellDetector implements
     @Override
     public SmellReport detectSmell(HashMap<Class, File> classes) {
 
-
         SmellReport smells = new SmellReport();
 
-
         for (Class current : classes.keySet()) {    //iterate through all the classes in the hashmap
-        List<String> publicFields = new ArrayList<>();
-        List<Integer> lines = new ArrayList<>(); //list of line numbers where smell was found
-             Field[] fields = current.getDeclaredFields();
-            for(Field f: fields) {
+            List<String> publicFields = new ArrayList<>();
+            Set<Integer> lines = new HashSet<>(); //list of line numbers where smell was found
+            Field[] fields = current.getDeclaredFields();
+            for (Field f : fields) {
                 int modifiers = f.getModifiers();
 
-                // System.out.println(f);
                 if (Modifier.isPublic(modifiers)) {  //check that the field is public
-
                     publicFields.add(f.getName());
                 }
-                    try {
-                        int count = 1;
-                        FileReader targetStream = new FileReader(classes.get(current));
 
-                        BufferedReader bufferedReader =
-                                new BufferedReader(targetStream);
-                        String line = bufferedReader.readLine();
+                try {
+                    int lineNumber = 1;
+                    FileReader targetStream = new FileReader(classes.get(current));
 
-                        while (line != null) {
-                            for (String name : publicFields){
-                            if (line.contains(name) && line.contains("public")){ //if the line is a public declaration
+                    BufferedReader bufferedReader =
+                            new BufferedReader(targetStream);
+                    String line = bufferedReader.readLine();
 
-                                publicFields.remove(name);  //remove the field from the list to avoid duplicates
-                                lines.add(count); //add to line number list
+                    while (line != null) {
+                        for (String name : publicFields) {
+                            if (line.contains(name) && line.contains("public")) { //if the line is a public declaration
+
+                                //publicFields.remove(name); //remove the field from the list to avoid duplicates
+                                lines.add(lineNumber); //add to line number list
                             }
-                            }
-                            line = bufferedReader.readLine();
-                            count++;
                         }
-
-                    } catch (Exception e) {
-
-                        System.out.println("Invalid file");
+                        line = bufferedReader.readLine();
+                        lineNumber++;
                     }
-
-
+                } catch (Exception e) {
+                    System.err.println("Invalid file" + e.toString());
                 }
-            smells.addToReport(classes.get(current), lines); //add the current file and list of lines to the report
             }
 
+            ArrayList<Integer> finalLines = new ArrayList<>(lines);
 
+            smells.addToReport(classes.get(current), finalLines); //add the current file and list of lines to the report
+        }
 
         return smells;
     }
