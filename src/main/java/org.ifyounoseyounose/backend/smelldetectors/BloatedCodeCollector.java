fe7153c818648d4f2992f538957a16f54a15a0b2
SineadFarrell <sinead.farrell1@ucdconnect.ie>
2019-04-10 10:23:59 +0100

    Start working on BloatedCodeSmellDetector

---
+++ src/main/java/org.ifyounoseyounose/backend/smelldetectors/BloatedCodeCollector.java
@@ -0,0 +1,39 @@
+package org.ifyounoseyounose.backend.smelldetectors;
+import java.util.List;
+
+import com.github.javaparser.Range;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+import java.lang.reflect.Type;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+public class BloatedCodeCollector extends VoidVisitorAdapter<List<Integer>> {
+
+    @Override
+    public void visit(MethodDeclaration md, List<Integer> collector){
+        super.visit(md, collector);
+        int limit = 50;
+
+        if(md.getRange().get().end.line - md.getRange().get().begin.line >= limit){
+            addLineNumbers(md, collector);
+        }
+
+
+    }
+
+
+
+    private void addLineNumbers(Node node, List<Integer> collector) {
+        Optional<Range> m = node.getRange();
+        Range r = m.get();
+        for (int lineNumber = r.begin.line; lineNumber <= r.end.line; lineNumber++) {
+            collector.add(lineNumber);
+        }
+    }
+
+}
