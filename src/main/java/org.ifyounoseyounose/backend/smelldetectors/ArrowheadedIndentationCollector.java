b532fdd20957c1318965ebde3415ee7a4a57fd7b
Oisin Quinn <oisin1001@gmail.com>
2019-04-10 19:38:59 +0100

    Started work on detecting arrowheaded indentation. Just need to test it now.

---
+++ src/main/java/org.ifyounoseyounose/backend/smelldetectors/ArrowheadedIndentationCollector.java
@@ -0,0 +1,37 @@
+package org.ifyounoseyounose.backend.smelldetectors;
+
+import com.github.javaparser.Range;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.stmt.IfStmt;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ArrowheadedIndentationCollector extends VoidVisitorAdapter<List<Integer>> {
+    private int depth = 0;
+    private int limit;
+
+    void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    @Override
+    public void visit(IfStmt md, List<Integer> collector) {
+        depth++;
+        if (depth >= limit) {
+            addLineNumbers(md, collector);
+        } else {
+            super.visit(md, collector);
+        }
+        depth--;
+    }
+
+    private void addLineNumbers(Node node, List<Integer> collector) {
+        Optional<Range> m = node.getRange();
+        Range r = m.get();
+        for (int lineNumber = r.begin.line; lineNumber <= r.end.line; lineNumber++) {
+            collector.add(lineNumber);
+        }
+    }
+}
---
+++ src/main/java/org.ifyounoseyounose/backend/smelldetectors/ArrowheadedIndentationSmellDetector.java
@@ -0,0 +1,37 @@
+package org.ifyounoseyounose.backend.smelldetectors;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+import org.ifyounoseyounose.backend.SmellReport;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ArrowheadedIndentationSmellDetector extends LimitableSmellDetector implements JavaParserSmellDetector {
+
+    public ArrowheadedIndentationSmellDetector(int limit) {
+        super(limit);
+    }
+
+    public ArrowheadedIndentationSmellDetector() {
+        super(3);
+    }
+
+    @Override
+    public SmellReport detectSmell(HashMap<CompilationUnit, File> compilationUnits) {
+        SmellReport smellReport = new SmellReport();
+        ArrowheadedIndentationCollector visitor = new ArrowheadedIndentationCollector();
+         visitor.setLimit(limit);
+
+        // We check for literals in each individual CompliationUnit and record the line numbers of instances of literals being used
+        for (CompilationUnit compilationUnit : compilationUnits.keySet()) {
+            List<Integer> collector = new ArrayList<>();
+            visitor.visit(compilationUnit, collector);
+            smellReport.addToReport(compilationUnits.get(compilationUnit),collector);
+        }
+
+        return smellReport;
+    }
+}
