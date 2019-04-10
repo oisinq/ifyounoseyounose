efa2c023ba0871696accb4c350e9a101f24e1a50
charliekelly13 <charleskelly13@gmail.com>
2019-04-10 17:07:19 +0100

    Attempting to work typesolver. Currently not working

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module org.ifyounoseyounose {
     requires java.compiler;
     requires com.github.javaparser.core;
     requires  guava;
-
+    requires com.github.javaparser.symbolsolver.core;
     opens org.ifyounoseyounose to javafx.fxml;
     exports org.ifyounoseyounose;
 }
--- src/main/java/org.ifyounoseyounose/backend/smelldetectors/DeadCodeSmellDetector.java
+++ src/main/java/org.ifyounoseyounose/backend/smelldetectors/DeadCodeSmellDetector.java
@@ -1,7 +1,14 @@
 package org.ifyounoseyounose.backend.smelldetectors;
 
+import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.MethodDeclaration;
+
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
+
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 import com.github.javaparser.ast.visitor.VoidVisitor;
 import org.ifyounoseyounose.backend.SmellReport;
 
@@ -15,7 +22,7 @@ public class DeadCodeSmellDetector extends SmellDetector implements JavaParserSm
     public SmellReport detectSmell(HashMap<CompilationUnit, File> compilationUnits){
         SmellReport smells = new SmellReport();
             VoidVisitor<List<MethodDeclaration>> visitor1 = new DeadCodeMethodCollector();
-            VoidVisitor<List<Integer>> visitor2 = new DeadCodeObjectCollector();
+            VoidVisitor<List<VariableDeclarationExpr >> visitor2 = new DeadCodeObjectCollector();
             HashMap<File, List<MethodDeclaration>> methodHash = new HashMap<>();
             for(CompilationUnit comp: compilationUnits.keySet()){
                 List<MethodDeclaration> methodList = methodHash.get(compilationUnits.get(comp)); //see if you already have a hashmap for current key
@@ -25,8 +32,17 @@ public class DeadCodeSmellDetector extends SmellDetector implements JavaParserSm
                     methodHash.put(compilationUnits.get(comp),methodList);
                 }
             visitor1.visit(comp, methodHash.get(compilationUnits.get(comp)));
-            System.out.println(methodHash.get(compilationUnits.get(comp)).get(1).getDeclarationAsString());
             }
+            List<VariableDeclarationExpr> temp = new ArrayList<>();
+        for(CompilationUnit comp: compilationUnits.keySet()) {
+            CombinedTypeSolver combinedTypeSolver = new CombinedTypeSolver();
+            combinedTypeSolver.add(new ReflectionTypeSolver());
+            JavaSymbolSolver symbolSolver = new JavaSymbolSolver(combinedTypeSolver);
+            StaticJavaParser.getConfiguration().setSymbolResolver(symbolSolver);
+
+            visitor2.visit(comp, temp);
+
+        }
         return null;
 
     }
