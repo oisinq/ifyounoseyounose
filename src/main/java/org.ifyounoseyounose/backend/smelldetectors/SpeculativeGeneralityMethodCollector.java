79f7c8e51e8886a516c57c5ae3352245ef5ed3aa
charliekelly13 <charleskelly13@gmail.com>
2019-04-16 21:27:50 +0100

    Testing ways to detect methods containing only comments

--- src/main/java/org.ifyounoseyounose/backend/smelldetectors/SpeculativeGeneralityMethodCollector.java
+++ src/main/java/org.ifyounoseyounose/backend/smelldetectors/SpeculativeGeneralityMethodCollector.java
@@ -14,20 +14,8 @@ public class SpeculativeGeneralityMethodCollector extends VoidVisitorAdapter<Lis
     @Override
     public void visit(MethodDeclaration md, List<Integer> collector) {
         super.visit(md, collector);
-        //System.out.println(md);
-        MethodDeclaration m1=md;
-        String methodBody="";
-        for (Comment child : m1.getAllContainedComments()) {
-            System.out.println(m1);
-            System.out.println(m1.remove(child));
-            System.out.println(m1);
-            methodBody = m1.getBody().toString();
-            if(methodBody.substring(10, md.getBody().toString().length() - 2).trim().length()==0)
-            {
-                addLineNumbers(md, collector);
-            }
-            //System.out.println("Removed child"+child+"Method"+methodBody);
-        }
+
+
             String s = md.getBody().toString().substring(10, md.getBody().toString().length() - 2);
             if ((s.trim().length() == 0) || s.contains("TO DO") || s.contains("TODO") || s.contains("todo") || s.contains("to do")) {
                 addLineNumbers(md, collector);
