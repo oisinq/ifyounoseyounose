90fe90f4921c0dcb26c537ca7edb41c0e158e7d1
charliekelly13 <charleskelly13@gmail.com>
2019-04-21 03:24:49 +0100

    Code commented. Cannot handle comment only methods

--- src/main/java/org.ifyounoseyounose/backend/smelldetectors/SpeculativeGeneralityMethodCollector.java
+++ src/main/java/org.ifyounoseyounose/backend/smelldetectors/SpeculativeGeneralityMethodCollector.java
@@ -12,21 +12,16 @@ import java.util.Optional;
 
 public class SpeculativeGeneralityMethodCollector extends VoidVisitorAdapter<List<Integer>> {
     @Override
-    public void visit(MethodDeclaration md, List<Integer> collector) {
+    public void visit(MethodDeclaration md, List<Integer> collector) {// Collects method declarations and returns their lines
         super.visit(md, collector);
-        String mdString;
-        for (Comment child : md.getAllContainedComments()) {
-            md.remove(child);
-            mdString = md.getBody().toString();
-            System.out.println(mdString);
-        }
-            String s = md.getBody().toString().substring(10, md.getBody().toString().length() - 2);
-            if ((s.trim().length() == 0) || s.contains("TO DO") || s.contains("TODO") || s.contains("todo") || s.contains("to do")) {
-                addLineNumbers(md, collector);
+
+            String s = md.getBody().toString().substring(10, md.getBody().toString().length() - 2);//Gets the simple string version of method
+            if ((s.trim().length() == 0) || s.contains("TO DO") || s.contains("TODO") || s.contains("todo") || s.contains("to do")) {//Checks if the method is empty of contains todo
+                addLineNumbers(md, collector);//Collects the line numbers
             }
         }
 
-    private void addLineNumbers(Node node, List<Integer> collector) {//Gets line number of input
+    private void addLineNumbers(Node node, List<Integer> collector) {//Gets line number of the method declaration 
         Optional<Range> m = node.getRange();
         Range r = m.get();
 
