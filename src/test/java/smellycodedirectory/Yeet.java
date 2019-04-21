f40eb1693da6403fac677a5f051a5cb8f7c453c1
charliekelly13 <47389478+charliekelly13@users.noreply.github.com>
2019-04-21 02:07:43 +0100

    Update Yeet.java

--- src/test/java/smellycodedirectory/Yeet.java
+++ src/test/java/smellycodedirectory/Yeet.java
@@ -1,14 +1,10 @@
 package smellycodedirectory;
 
 public class Yeet {
-TemporaryFieldsSmellDetector
     int a =0;
-
     public void lol() {
-        reeeeee l = new reeeeee();
-        System.out.println(l.x().y().z());
+        System.out.println("hup the lads");
         double i;
- TemporaryFieldsSmellDetector
         i=1+1;
         i=0;
 
@@ -20,17 +16,5 @@ TemporaryFieldsSmellDetector
         return a;
     }
 
-}
-        i = 1 + 1;
-        i = 0;
-        //mmer(lol.ha().ha().ha());
-        reeeeee djdj = new reeeeee();
-        djdj.z().x().y();
-    }
-    public class reeeeee{
-        public reeeeee x(){return new reeeeee();}
-        public reeeeee z(){return new reeeeee();}
-        public reeeeee y(){return new reeeeee();}
-    }
 }
 
